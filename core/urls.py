c87c84cb989d8723ff2c6e61c90e3628b8169177
joelproxi <joeledmond95@gmail.com>
2023-11-11 14:17:31 +0100

    clear project

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from . import views
-
-
-routeur = DefaultRouter()
-routeur.register('', views.VideoViewset, basename='vidoes')
-
-urlpatterns = routeur.urls
--- core/utils.py
+++
@@ -1,9 +0,0 @@
-import pika
-
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host='localhost'
-    )
-)
-channel = connection.channel()
