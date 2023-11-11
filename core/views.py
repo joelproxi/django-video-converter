baca8bade5b4f09a1194d7bd161ed7e38f87e4cf
joelproxi <joeledmond95@gmail.com>
2023-11-11 15:06:10 +0100

    Pusblish message to queue video

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-from rest_framework import viewsets, status
+import json
+import pika
+import os
+
+
+from rest_framework import viewsets, status, exceptions
 from rest_framework.response import Response
 
 from core.models import VideoFile
 from core.serializers import VideoSerializer
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'
+    )
+)
+channel = connection.channel()
 
 
 class VideoViewset(viewsets.ModelViewSet):
@@ -14,5 +26,31 @@ class VideoViewset(viewsets.ModelViewSet):
         data = request.data
         serializer = VideoSerializer(data=data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(video_data=data.get('video').file.read())
+        video = serializer.save(video_data=data.get('video').file.read())
+
+        message = {
+            'video_id': str(video.pk),
+            'audio_id': None,
+            'email': 'joeledmond95@yahoo.com'
+        }
+
+        try:
+            channel.queue_declare(queue='video', durable=True)
+            channel.basic_publish(
+                exchange='',
+                routing_key='video',
+                body=json.dumps(message),
+                properties=pika.BasicProperties(
+                    delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+                )
+            )
+            channel.close()
+        except Exception as err:
+            print(err)
+            # instance = get_object_or_404(VideoFile, pk=video.pk)
+            # instance.delete()
+            self.perform_destroy(video)
+            raise exceptions.APIException("Internal server error")
+
+        os.remove(str(video.video))
         return Response(serializer.data, status=status.HTTP_201_CREATED)
