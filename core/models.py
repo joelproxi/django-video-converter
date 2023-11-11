02e59d6b9471a3d60f65510bad09495f6fccd694
joelproxi <joeledmond95@gmail.com>
2023-11-11 14:25:24 +0100

    add video model

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class VideoFile(models.Model):
+    video = models.FileField()
+    video_data = models.BinaryField(null=True)
+
+    def __str__(self) -> str:
+        return self.pk
