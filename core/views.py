00fca42d9691b538cf37ba6f7588719f0a6b5559
joelproxi <joeledmond95@gmail.com>
2023-11-11 14:40:17 +0100

    override create method on viewset to add binary data

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from core.models import VideoFile
 from core.serializers import VideoSerializer
@@ -8,3 +9,10 @@ from core.serializers import VideoSerializer
 class VideoViewset(viewsets.ModelViewSet):
     queryset = VideoFile.objects.all()
     serializer_class = VideoSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = VideoSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(video_data=data.get('video').file.read())
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
