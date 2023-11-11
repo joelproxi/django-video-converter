a6bac8021c3249a62cc98ff8fda1211a019c3e34
joelproxi <joeledmond95@gmail.com>
2023-11-11 14:30:17 +0100

    add videos viewset

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import VideoFile
+from core.serializers import VideoSerializer
+
+
+class VideoViewset(viewsets.ModelViewSet):
+    queryset = VideoFile.objects.all()
+    serializer_class = VideoSerializer
