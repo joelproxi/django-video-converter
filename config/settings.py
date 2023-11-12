6126e6f726f26ed4959994101a93408a59ec8879
proxidev <joeledmond95@gmail.com>
2023-11-12 15:44:50 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'proxidev02@gmail.com'
-EMAIL_HOST_PASSWORD = "xrdu mkyu dtxl dtgr"
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""
